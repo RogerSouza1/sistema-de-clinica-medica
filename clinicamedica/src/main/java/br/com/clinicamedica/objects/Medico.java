aa768c80beaf357228773fd39e80e4748d66321a
RogerSouza1 <roger.7090@outlook.com>
2024-04-01 17:44:28 -0300

    Adiciona getters e setters para as classes Paciente, Disponibilidade, Agendamento, Medico, Usuario e Endereco

--- clinicamedica/src/main/java/br/com/clinicamedica/objects/Medico.java
+++ clinicamedica/src/main/java/br/com/clinicamedica/objects/Medico.java
@@ -6,4 +6,37 @@ public class Medico extends Usuario{
     private String CRM;
     private String especialidade;
     private String clinica;
+    
+    public int getIdMedico() {
+        return idMedico;
+    }
+    public void setIdMedico(int idMedico) {
+        this.idMedico = idMedico;
+    }
+    public Usuario getUsuario() {
+        return usuario;
+    }
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+    public String getCRM() {
+        return CRM;
+    }
+    public void setCRM(String cRM) {
+        CRM = cRM;
+    }
+    public String getEspecialidade() {
+        return especialidade;
+    }
+    public void setEspecialidade(String especialidade) {
+        this.especialidade = especialidade;
+    }
+    public String getClinica() {
+        return clinica;
+    }
+    public void setClinica(String clinica) {
+        this.clinica = clinica;
+    }
+
+    
 }
--- clinicamedica/src/main/java/br/com/clinicamedica/objects/Paciente.java
+++ clinicamedica/src/main/java/br/com/clinicamedica/objects/Paciente.java
@@ -4,5 +4,26 @@ public class Paciente extends Usuario{
     private int idPaciente;
     private Usuario usuario;
     private int denpendentes;
+    
+    public int getIdPaciente() {
+        return idPaciente;
+    }
+    public void setIdPaciente(int idPaciente) {
+        this.idPaciente = idPaciente;
+    }
+    public Usuario getUsuario() {
+        return usuario;
+    }
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+    public int getDenpendentes() {
+        return denpendentes;
+    }
+    public void setDenpendentes(int denpendentes) {
+        this.denpendentes = denpendentes;
+    }
+
+    
 
 }
