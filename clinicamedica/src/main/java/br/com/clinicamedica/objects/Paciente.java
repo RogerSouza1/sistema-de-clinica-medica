8a6c292c149363cfe24bb303abc9752bd3a8d5db
RogerSouza1 <roger.7090@outlook.com>
2024-04-02 13:09:39 -0300

    Atualizações nas classes Disponibilidade, Agendamento, Paciente, Medico e Usuario

--- clinicamedica/src/main/java/br/com/clinicamedica/objects/Paciente.java
+++ clinicamedica/src/main/java/br/com/clinicamedica/objects/Paciente.java
@@ -1,29 +1,32 @@
 package br.com.clinicamedica.objects;
 
-public class Paciente extends Usuario{
+public class Paciente extends Usuario {
     private int idPaciente;
     private Usuario usuario;
-    private int denpendentes;
-    
+    private int dependentes;
+
     public int getIdPaciente() {
         return idPaciente;
     }
+
     public void setIdPaciente(int idPaciente) {
         this.idPaciente = idPaciente;
     }
+
     public Usuario getUsuario() {
         return usuario;
     }
+
     public void setUsuario(Usuario usuario) {
         this.usuario = usuario;
     }
-    public int getDenpendentes() {
-        return denpendentes;
-    }
-    public void setDenpendentes(int denpendentes) {
-        this.denpendentes = denpendentes;
+
+    public int getDependentes() {
+        return dependentes;
     }
 
-    
+    public void setDependentes(int dependentes) {
+        this.dependentes = dependentes;
+    }
 
 }
--- clinicamedica/src/main/java/br/com/clinicamedica/objects/Usuario.java
+++ clinicamedica/src/main/java/br/com/clinicamedica/objects/Usuario.java
@@ -10,50 +10,61 @@ abstract class Usuario {
     private Date dataNascimento;
     private String telefone;
     private Endereco endereco;
-    
+
     public String getNome() {
         return nome;
     }
+
     public void setNome(String nome) {
         this.nome = nome;
     }
+
     public int getCPF() {
         return CPF;
     }
-    public void setCPF(int cPF) {
-        CPF = cPF;
+
+    public void setCPF(int CPF) {
+        this.CPF = CPF;
     }
+
     public String getEmail() {
         return email;
     }
+
     public void setEmail(String email) {
         this.email = email;
     }
+
     public String getSenha() {
         return senha;
     }
+
     public void setSenha(String senha) {
         this.senha = senha;
     }
+
     public Date getDataNascimento() {
         return dataNascimento;
     }
+
     public void setDataNascimento(Date dataNascimento) {
         this.dataNascimento = dataNascimento;
     }
+
     public String getTelefone() {
         return telefone;
     }
+
     public void setTelefone(String telefone) {
         this.telefone = telefone;
     }
+
     public Endereco getEndereco() {
         return endereco;
     }
+
     public void setEndereco(Endereco endereco) {
         this.endereco = endereco;
     }
-
-    
 
 }
